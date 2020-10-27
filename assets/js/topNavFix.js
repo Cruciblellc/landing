e7b2ed9b2c2bff6b6d105834de5a9e47a870a030
merlinlcb <merlinlcb@gmail.com>
2020-10-27 16:33:59 -0600

    update andrew.jpg

--- assets/js/topNavFix.js
+++
@@ -1,6 +0,0 @@
-// Takes care of the issue where the navigation tool tips don't go away after being clicked.
-$(function () {
-  $(document).on('click', '.nav-link', function() {
-    $(this).blur();
-  });
-});
